3665a4b349eaf2fe4b042aafdc3cde9f0335c80c
florihu <131660812+florihu@users.noreply.github.com>
2024-11-06 14:36:59 +0100

    pre tidying sp reading script

--- D_sp_data_clean.py
+++ D_sp_data_clean.py
@@ -3,63 +3,136 @@ from util import get_path
 
 import pandas as pd 
 import os
+import numpy as np
+from functools import reduce
 
-def get_site_data(site_name, var_exp_path):
-    '''
-    Get the site data, merges further additions
-
-    Parameters:
-        site_name (str): The name of the site folder
-    
-    Returns:
-        DataFrame: The site data
-    '''
-    collect = []
-
-    var_types_df = pd.read_excel(var_exp_path, sheet_name='site_keys', usecols='A:B')
-    # to dict
-    var_types = dict(zip(var_types_df['Var_name'], var_types_df['Dtype']))
-
-    for f in os.listdir(site_name):
-        site_data = pd.read_excel(os.path.join(site_name, f), skiprows=4)  
-
-        site_data.dropna(axis=0, how='all', inplace=True)
-
-        dup_removed = duplicate_columns_drop(site_data)
-
-        dtype_converted = dtype_conversion(dup_removed, var_types)
-
-        dtype_converted.set_index('PROP_ID', inplace=True)
-
-        collect.append(dtype_converted)
-    
-    if len(collect) == 1:
-        singular = duplicate_columns_drop(collect[0])
-        return singular
-    else:
-        merged_data = pd.merge(collect, how='outer', on='PROP_ID', axis=1)
-        merged_dropped = duplicate_columns_drop(merged_data)
-        return merged_dropped
-
-def duplicate_columns_drop(df, suffixes = ['.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9']):
-    '''
-    Identify duplicate columns in a DataFrame
+def get_data(folder_name, var_exp_path):
+    """
+    Get site data and merges additional entries.
 
     Parameters:
-        df (DataFrame): The DataFrame to check for duplicates
-    
-    Returns:
-        list: A list of duplicate columns
-    '''
-    suf_cols = [col for col in df.columns if any(col.endswith(suffix) for suffix in suffixes)]
+        folder_name (str): The name of the site folder.
+        var_exp_path (str): The path to the variable explanation file.
 
-    dup_cols = df.columns[df.columns.duplicated(keep='first')].tolist()
+    Returns:
+        DataFrame: The merged or singular site data.
+    """
     
-    # Drop identified columns
-    df_dropped = df.drop(columns=suf_cols+dup_cols)
+    folder_path = get_path(folder_name)
+
+    if folder_name in ['site', 'site_temp', 'site_com']:
+        collect = []
+        for f in os.listdir(folder_path):
+            site_data = pd.read_excel(os.path.join(folder_path, f), skiprows=4).dropna(how='all')
+
+            id_col_index = np.where(site_data.columns.str.contains('PROP_ID', case=False))[0][0]
+
+            if folder_name == 'site':
+                id_cols = ['PROP_ID']
+                dtypes = site_data.iloc[0].astype(str)
+                site_data = site_data.iloc[id_col_index:]
+                var_types = dict(zip(site_data.columns, dtypes))
+
+            elif folder_name == 'site_com':
+                id_cols = ['PROP_ID']
+                com = f.split('_')[-1].split('.')[0]
+                dtypes = site_data.iloc[0].astype(str)
+                var_types = dict(zip(site_data.columns, dtypes))
+
+                site_data = site_data.iloc[1:,id_col_index:]
+                site_data['COMMODITY'] = com
+                
+                var_types['COMMODITY'] = 'str'
+
+            elif folder_name == 'site_temp':
+                id_cols = ['PROP_ID', 'YEAR']
+                site_data, var_types = col_trans(site_data, id_col_index)
+        
+
+            dtype_converted = dtype_conversion(site_data, var_types)
+            dtype_converted.set_index(id_cols, inplace=True)
+            collect.append(dtype_converted)
+        
+    if folder_name in  ['site', 'site_temp']:
+        merged = reduce(lambda left, right: pd.merge(left, right, how='outer', on=id_cols), collect).reset_index()
+        return merged
+    elif folder_name == 'site_com':
+        if len(collect) == 1:
+            return collect[0]
+        else:
+            # sort al dfs in conc by columns
+            conc = [df.sort_index(axis=1) for df in collect]
+            # assert columns are the same in all dfs that are concated
+            assert all([collect[0].columns == c.columns for c in collect[1:]])
+            return pd.concat(collect, axis=0)
+
+    elif folder_name == 'site_temp_com':
+            conc = []
+            for f in os.listdir(folder_path):
+                collect = []
+                commodity = f.split('/')[-1]
+                id_cols = ['PROP_ID', 'YEAR']
+                com_path = os.path.join(folder_path, f)
+                for f_ in os.listdir(com_path):
+                    site_data = pd.read_excel(os.path.join(com_path, f_), skiprows=4).dropna(how='all')
+                    
+                    id_col_index = np.where(site_data.columns.str.contains('PROP_ID', case=False))[0][0]
+                    
+                    site_data, var_types = col_trans(site_data, id_col_index)
+
+                    dtype_converted = dtype_conversion(site_data, var_types)
+                    dtype_converted.set_index(id_cols, inplace=True)
+                    collect.append(dtype_converted)
+
+                merged = reduce(lambda left, right: pd.merge(left, right, how='outer', on=id_cols), collect).reset_index()
+                
+                merged_reset = merged.reset_index() 
+
+                merged_reset['COMMODITY'] = commodity
+
+                conc.append(merged_reset)
+            
+            if len(conc) == 1:
+                return conc[0]
+            else: 
+                # sort al dfs in conc by columns
+                conc = [df.sort_index(axis=1) for df in conc]
+                # assert columns are the same in all dfs that are concated
+                assert all([conc[0].columns == c.columns for c in conc[1:]])
+                return pd.concat(conc, axis=0)
+
     
-    return df_dropped
-    
+
+
+
+def col_trans(site_data, id_col_index):
+    # Assume the third column as the variable name
+    var_name = site_data.columns[id_col_index + 1]
+
+    # Set headers based on the first row and adjust columns
+    site_data.columns = site_data.iloc[0]
+    site_data = site_data.iloc[1:, id_col_index:]
+    site_data.rename(columns={site_data.columns[id_col_index]: 'PROP_ID'}, inplace=True)
+    site_data = site_data.dropna(subset=['PROP_ID'])
+
+    # Stack data with 'PROP_ID' and 'YEAR' as indexes, renaming the variable column
+    stacked = site_data.set_index('PROP_ID').stack()
+
+    stacked.index.set_names(['PROP_ID', 'YEAR'], inplace=True)
+
+    stacked_df = stacked.reset_index().rename({0: var_name}, axis=1)
+
+    # Clean 'YEAR' column by removing 'Y' prefix
+    stacked_df['YEAR'] = stacked_df['YEAR'].str.replace('Y', '', regex=False).astype(int)
+
+    # Define variable types based on exceptions or defaults
+    var_types = {'YEAR': 'date', 'PROP_ID': 'int', var_name: exept_vars.get(var_name, default_dtype)}
+
+    return stacked_df, var_types
+
+
+
+
 
 def dtype_conversion(df, type_dict):
     '''
@@ -77,13 +150,22 @@ def dtype_conversion(df, type_dict):
         dtype = type_dict.get(c, None)
 
         if dtype == 'date':
-            df[c] = pd.to_datetime(df[c], errors='coerce')
+
+            df[c] = pd.to_numeric(df[c], errors='coerce')
+            df = df[(df[c] >= 1677) & (df[c] <= 2262)]  # Filter rows with valid years
+
+            # Convert to datetime, handling potential issues with mixed formats
+            df[c] = pd.to_datetime(df[c].astype(str), format='%Y', errors='coerce')
+            
         elif dtype == 'int':
             df[c] = pd.to_numeric(df[c], errors='coerce').astype('Int64')
         elif dtype == 'float':
             df[c] = pd.to_numeric(df[c], errors='coerce')
         elif dtype == 'str':
             df[c] = df[c].astype('str')
+
+        else:
+            AssertionError('The data type could not be assigned')
 
     return df
 
@@ -113,14 +195,12 @@ def get_site_com_files():
     '''
     return None
 
+
+exept_vars = {'MODEL_EST_DATE': 'date'}
+default_dtype = 'float'
+
 if __name__ == '__main__':
 
     var_exp_path = get_path('sp_variable_description.xlsx')
 
-    site_folder_path = get_path('site')
-
-    temp_folder_path = get_path('site_temp')
-    
-    com_folder_path = get_path('site_temp_com')
-
-    site = get_site_data(site_folder_path, var_exp_path)
+    site_folder_path = get_data('site', var_exp_path)
